aaf4bc3f04be75c34ef494cda4d704616d2723f8
ConorMacFadden <90274527+ConorMacFadden@users.noreply.github.com>
2023-04-01 19:24:29 +0100

    Add playRound function
    The playRound function takes the playerChoice and computerChoice inputs
    as strings and returns "win", "draw" or "lose" as appropriate for the
    player. This version can handle the inputs "Rock", "Paper" or "Scissors"
    in any case but it does not check for any invalid choices.

--- rpsGame.js
+++ rpsGame.js
@@ -11,5 +11,40 @@ function getComputerChoice(){
         case 2:
             return "Scissors"
     }
+}
+
+function playRound(playerSelection, computerSelection){
+    let outcomeMessage = "";
+    let computerSelectionUpperCase = computerSelection.toUpperCase();
+
+    let playerWinStatus = "draw";
+
+    switch(playerSelection.toUpperCase()){
+        case "ROCK":
+            if (computerSelectionUpperCase ==="PAPER"){ 
+                playerWinStatus = "Lose";
+            } else if (computerSelectionUpperCase ==="SCISSORS") {
+                playerWinStatus = "Win";
+            }
+            break;
+
+        case "PAPER":
+            if (computerSelectionUpperCase ==="SCISSORS"){ 
+                playerWinStatus = "Lose";
+            } else if (computerSelectionUpperCase ==="ROCK") {
+                playerWinStatus = "Win";
+            }
+            break;
+
+        case "SCISSORS":
+            if (computerSelectionUpperCase ==="ROCK"){ 
+                playerWinStatus = "Lose";
+            } else if (computerSelectionUpperCase ==="PAPER") {
+                playerWinStatus = "Win";
+            }
+            break;
+    }
+
+    return playerWinStatus;
 
 }
